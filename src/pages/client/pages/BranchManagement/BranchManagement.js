fb311e063be82e50a763431a7d130f3b9dc8dcdb
BASAVA CHETAN <81961559+Basavachetan09@users.noreply.github.com>
2022-05-23 15:46:09 +0530

    added Branch management component in client

---
+++ src/pages/client/pages/BranchManagement/BranchManagement.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BranchManagement = () => {
+    return (
+        <div>
+            BranchManagement
+        </div>
+    )
+}
+
+export default BranchManagement;
